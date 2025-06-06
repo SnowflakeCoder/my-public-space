79cd4a4b7a06c246983dab9351723555eacca072
Arun <arun00011@gmail.com>
2025-06-06 09:48:03 +0200

    Added Swagger config to build.gradle

--- projects/tdd-service/src/test/java/com/myspace/tdd/TddServiceApplicationTests.java
+++ projects/tdd-service/src/test/java/com/myspace/tdd/TddServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.myspace.tdd;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TddServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
