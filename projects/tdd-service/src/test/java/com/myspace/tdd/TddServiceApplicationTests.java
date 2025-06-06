6dc75ef51a60cfe7a9851aa72504a23760883c12
Arun <arun00011@gmail.com>
2025-06-06 08:45:25 +0200

    commiting basic project

---
+++ projects/tdd-service/src/test/java/com/myspace/tdd/TddServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspace.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
